fd7e90c7fba80a7841fae7ca52f5f34a1be05e87
wangpengcheng <1787561180@qq.com>
2023-06-08 09:58:41 +0800

    fast commit

---
+++ src/main/java/com/example/wpclc/WpcLcApplication.java
@@ -0,0 +1,13 @@
+package com.example.wpclc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WpcLcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WpcLcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wpclc/WpcLcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wpclc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WpcLcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
